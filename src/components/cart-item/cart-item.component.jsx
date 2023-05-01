18ab2dd319f9f56d9f2230a193e8d33b8029dcfd
Jordan Layne <jordanmlayne2001@gmail.com>
2023-05-01 14:09:24 -0400

    created checkout functionality

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -2,7 +2,8 @@ import './cart-item.styles.scss'
 
 
 const CartItem = ({ cartItem }) => {
-    const { imageUrl, price, name, quantity } = cartItem;
+  
+const {name,imageUrl,price,quantity} =cartItem
   
     return (
       <div className='cart-item-container'>
